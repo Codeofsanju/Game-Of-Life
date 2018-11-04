932ae21cdab327683e7e5c4b95eac59d308270ae
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-04 13:58:38 -0500

    livingNeighbors fixed

--- GameOfLife.js
+++ GameOfLife.js
@@ -18,17 +18,17 @@ class GameOfLife {
       }
       grid.push(row);
     }
-    grid[3][4] = 1;
     console.dir(grid);
     return grid;
   }
 
     getCell(row, col){
-      const args = Array.from(arguments);
-      if (args.length !== 2 || row > this.width - 1 || col > this.height - 1 || row < 0 || col < 0){
+      if(row > this.height -1 || col > this.width -1 || row < 0 || col < 0){
         return 'error';
       }
-      return this.board[row][col] === 0 ? 'dead':'alive';
+      else{
+        return this.board[row][col] === 0 ? 'dead':'alive';
+      }
     }
 
     setCell(value, row, col) {
@@ -37,6 +37,7 @@ class GameOfLife {
         return 'error';
       }
       this.board[row][col] = value;
+      
     }
     
     toggleCell(row, col) {
@@ -57,68 +58,29 @@ class GameOfLife {
 
   livingNeighbors(row, col) {
     // TODO: Return the count of living neighbors.
-    let topLeft = 0;
-    let topDir = 0;
-    let topRigh = 0;
-    let left = 0;
-    let right = 0;
-    let botLeft = 0;
-    let botDir = 0;
-    let botRight = 0;
-    
-    if(row === 0 && col === 0){
-        return this.board[row][col+1] + this.board[row+1][col+1] + this.board[row+1][col];
-    }
-    else if(row === 0 && col === width){
-        return this.board[row][col-1]+this.board[row+1][col-1]+this.board[row+1][col];
-    }
-    else if(row === height && col === 0){
-        topDir = this.board[row -1][col];
-        topRight = this.board[row-1][col+1];
-        return topDir+topRight+right;
-    }
-    else if(row === height && col === width){
-        topDir = this.board[row -1][col];
-        topLeft = this.board[row -1][col -1];
-        left = this.board[row][col-1];
-        return topDir+topLeft+left;
-    }
-    // middle of row 0 and row height
-    else if(row === 0 || row === height){
-        if(row === 0){
-            return left+botLeft+botDir+botRight+right;
+      const neighborsObj = {
+        topLeft: this.getCell(row - 1, col - 1),
+        topDir: this.getCell(row - 1, col),
+        topRight: this.getCell(row - 1, col + 1),
+        botLeft: this.getCell(row + 1, col - 1),
+        botDir: this.getCell(row + 1, col),
+        botRight: this.getCell(row + 1, col + 1),
+        right: this.getCell(row, col + 1),
+        left: this.getCell(row, col - 1),
+      };
+      let living = 0;
+  
+      for (let key in neighborsObj) {
+        if (neighborsObj[key] === 'alive') {
+          living++;
         }
-        else if(row === height){
-            return left+topLeft+topDir+topRight+right;
+        else{
+          living += 0;
         }
-    }
-    // middle of column 0 and column width
-    else if(col === 0 || col === width){
-        if(col === 0){
-            return topDir+topRight+right+botRight+botDir;
-        }
-        if(col === width){
-            return topDir+topLeft+left+botLeft+botDir;
-        }
-    }
-    else{
-        topLeft = this.board[row -1][col -1];
-        topDir = this.board[row -1][col];
-        topRight = this.board[row-1][col+1];
-        left = this.board[row][col-1];
-        right = this.board[row][col+1];
-        botLeft = this.board[row+1][col-1];
-        botDir = this.board[row+1][col];
-        botRight = this.board[row+1][col+1];
-        return topDir+topRight+right+botRight+botDir+botLeft+left+topLeft;
-    }
+      }
+      return living;
   }
 
-if col = 0;
-if col = width;
-if row = 0;
-if row = length;
-if row = col;
   /**
    * Given the present board, apply the rules to generate a new board
    */
@@ -136,7 +98,9 @@ if row = col;
     // based on their current alive neighbors
     for(let i = 0; i < this.height; i++){
       for(let j = 0; j < this.width; j++){
-        console.log(this.board[i][j]);
+        if(this.livingNeighbors(i, j) === 3){
+          console.log('need to die');
+        }
       }
     }
     this.board = newBoard;
